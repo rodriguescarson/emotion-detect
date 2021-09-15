ca36dc990ad9c452071df1b0b8fddef7761a8612
Carson <rodriguescarson@hotmail.com>
2021-09-15 22:49:13 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
 
-function App() {
+const emojiDictionary = {
+  "😊": "smiling!",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance"
+};
+console.log(Object.keys(emojiDictionary));
+var emojiweknow = Object.keys(emojiDictionary);
+var userName = "Carson";
+
+// var userName = prompt("Enter your name:");
+var color = "blue";
+
+export default function App() {
+  // const [likeCounter, setLikeCounter] = useState(0);
+
+  // function likeClickhandler() {
+  //   setLikeCounter(likeCounter + 1);
+  // }
+  // const [userInput, setUserInput] = useState("");
+  // function inputChangeHandler(event) {
+  //   setUserInput(event.target.value);
+  // }
+
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "we dont have this in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    if (meaning === undefined) {
+      meaning = "we dont have this in our database";
+    }
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ backgroundColor: color }}>Welcome {userName}</h1>
+      {/* <button onClick={likeClickhandler}>{likeCounter}</button>
+       */}
+      {/* <input onChange={inputChangeHandler}></input>
+      <div>Welcome {userInput}</div> */}
+      <input onChange={emojiInputHandler} />
+      <h2>{meaning}</h2>
+      <h3>emoji we know</h3>
+
+      {emojiweknow.map((emoji) => {
+        return (
+          <span
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+            onClick={() => emojiClickHandler(emoji)}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
